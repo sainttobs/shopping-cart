9518a79efcb4afed39620b1d3960d290ad1e4345
sainttobs <sainttobs@gmail.com>
2018-08-17 00:55:48 +0100

    First Commit

---
+++ seed/product-seeder.js
@@ -0,0 +1,58 @@
+var Product = require('../models/product');
+
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/shopping');
+
+var connection = mongoose.connection;
+connection.on('connected', function(){
+    console.log('connected to db');
+});
+
+var products = [
+	new Product({
+		imagePath: 'https://upload.wikimedia.org/wikipedia/en/thumb/a/a5/Grand_Theft_Auto_V.png/220px-Grand_Theft_Auto_V.png',
+		title: 'Grand_Theft_Auto_V',
+		description: 'Very Awesome Game!!!!!!',
+		price: 50
+	}),
+	new Product({
+		imagePath: 'https://images-na.ssl-images-amazon.com/images/I/81USj-o7CvL._AC_SL1500_.jpg',
+		title: 'Fortnite',
+		description: 'Very Interesting Game!!!!!!',
+		price: 70
+	}),
+	new Product({
+		imagePath: 'https://upload.wikimedia.org/wikipedia/en/thumb/1/15/The_Elder_Scrolls_V_Skyrim_cover.png/220px-The_Elder_Scrolls_V_Skyrim_cover.png',
+		title: 'The_Elder_Scrolls_V_Skyrim',
+		description: 'Incredible Game!!!!!!',
+		price: 80
+	}),
+	new Product({
+		imagePath: 'https://upload.wikimedia.org/wikipedia/en/thumb/1/1b/Monster_Hunter_World_cover_art.jpg/220px-Monster_Hunter_World_cover_art.jpg',
+		title: 'Monster_Hunter_World',
+		description: 'Very Awesome Game!!!!!!',
+		price: 100
+	}),
+	new Product({
+		imagePath: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/d4/FIFA18cover.png/220px-FIFA18cover.png',
+		title: 'FIFA18',
+		description: 'Very Awesome Game!!!!!!',
+		price: 150
+	})
+];
+
+var done = 0;
+for (var i = 0; i < products.length; i++) {
+	products[i].save(function (err, result) {
+		done++;
+		if (done === products.length){
+			exit();
+		}
+	});
+}
+
+
+function exit () {
+	mongoose.disconnect();
+}
